931cf0654ccc03786a096e56c2b982c1dfc9baa3
daniela-kirilova <daniela.kirilova@gmx.de>
2020-08-24 21:05:37 +0200

    game is done

--- tetris.js
+++ tetris.js
@@ -4,19 +4,94 @@ const context = canvas.getContext('2d')
 context.scale(20, 20)
 
 
-// draw pieces 
+function arenaSweep() {
+    let rowCount = 1
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer
+            }
+        }
+        //take the row at index y and filles it with 0  and added it in the top of the arena
+        const row = arena.splice(y, 1)[0].fill(0)
+        arena.unshift(row);
+        ++y;
 
-// T- Piece
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0],
-]
+        player.score += rowCount * 10;
+        rowCount *= 2;
+
+    }
+}
+
+function collide(arena, player) {
+    //if the raw doesnt exist we  still collide
+    const [m, o] = [player.matrix, player.position] //m=matrix, o=offset
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0) {
+                return true
+            }
+        }
+    }
+    return false
+}
+function createMatrix(w, h) {
+    const matrix = []
+    while (h--) {
+        matrix.push(new Array(w).fill(0))
+    }
+    return matrix;
+}
+function createPiece(type) {
+    if (type === "T") {
+        return [
+            [0, 0, 0],
+            [1, 1, 1],
+            [0, 1, 0],
+        ];
+    } else if (type === "O") {
+        return [
+            [2, 2],
+            [2, 2],
+        ];
+    } else if (type === "L") {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
+        ];
+    } else if (type === "J") {
+        return [
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
+        ];
+    } else if (type === "I") {
+        return [
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+        ];
+    } else if (type === "S") {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === "Z") {
+        return [
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0],
+        ];
+    }
+}
 
 function draw() {
-    context.fillStyle = '#000'
+    context.fillStyle = '#fff'
     context.fillRect(0, 0, canvas.width, canvas.height)
-
+    drawMatrix(arena, { x: 0, y: 0 })
     drawMatrix(player.matrix, player.position)
 }
 
@@ -25,30 +100,141 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red'
+                context.fillStyle = colors[value]
                 context.fillRect(x + offset.x, y + offset.y, 1, 1) // left, top, right, down
             }
         })
     })
 }
+// copy all the values from players into the arena
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.position.y][x + player.position.x] = value
+            }
+        })
+    })
+}
 //  make the pieces drop
-let dropCounter =0
-let dropInterval = 1000 //every 1 second that the piece drop
-let lastTime = 0 //last shown time so we can calculate timedependent things
+function playerDrop() {
+    player.position.y++
+    //if we are touching the ground or other piece 
+    if (collide(arena, player)) {
+        player.position.y--
+        merge(arena, player)
+        //when it touches the bottom it comes back from the top
+
+        playerReset();
+        arenaSweep();
+        updateScore()
+    }
+    dropCounter = 0
+}
+function playerMove(direction) {
+    player.position.x += direction
+    if (collide(arena, player)) {
+        player.position.x -= direction;
+    }
+}
+function playerReset() {
+    const pieces = "ILJOTSZ"
+    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+    player.position.y = 0;
+    player.position.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0)
+    if (collide(arena, player)) {
+        arena.forEach(row => row.fill(0));
+        player.score =0;
+        updateScore()
+
+    }
+}
+function playerRotate(direction) {
+    const position = player.position.x
+    let offset = 1
+    rotate(player.matrix, direction)
+    while (collide(arena, player)) {
+        player.position.x += offset
+        offset = -(offset + (offset > 0 ? 1 : -1))
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, - direction);
+            player.position.x = position
+            return;
+        }
+    }
+}
+function rotate(matrix, direction) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                    matrix[y][x],
+                    matrix[x][y],
+                ];
+        }
+    }
+    if (direction > 0) {
+        matrix.forEach(row => row.reverse())
+    } else {
+        matrix.reverse()
+    }
+}
+
+let dropCounter = 0
+let dropInterval = 1000 //every 1 second the piece drop
+let lastTime = 0
 function update(time = 0) {
     const deltaTime = time - lastTime
     lastTime = time
-dropCounter += deltaTime{
-    
-}
 
+    //draw a piece that drop every second
+    dropCounter += deltaTime
+    if (dropCounter > dropInterval) {
+        playerDrop()
+    }
     draw()
     requestAnimationFrame(update)
 }
-const player = {
-    position: { x: 5, y: 5 },
-    matrix: matrix
+function updateScore() {
+    document.getElementById('score').innerText = "Score: " + player.score
 }
+const colors = [
+    null,
+    '#efff0b',
+    '#4500e2',
+    '#db7aee',
+    '#42EADDFF',
+    '#ffc8c8',
+    '#00ff00',
+    '#FC766AFF',
+]
+
+
+const arena = createMatrix(12, 20)
+const player = {
+    position: { x: 0, y: 0 },
+    matrix: null,
+    score: 0,
+}
+
+document.addEventListener('keydown', event => {
+    // 37= arrow left, 39= arrow right, 40= arrow down, 32=space
+    if (event.keyCode === 37) {
+        playerMove(-1)
+    } else if (event.keyCode === 39) {
+        playerMove(1)
+    } else if (event.keyCode === 40) {
+        playerDrop()
+    } else if (event.keyCode === 32) {
+        playerRotate(-1)
+    } else if (event.keyCode === 32) {
+        playerRotate(1)
+    }
+})
+playerReset()
+updateScore()
 update()
 
 
